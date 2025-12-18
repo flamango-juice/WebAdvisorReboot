1782e74b05eac79bfbe11d3992cd4bbaf7920ffa
Conrad Salazar <78174372+TheMetroid7998@users.noreply.github.com>
2025-12-18 14:18:19 -0800

    Modify port again

--- webadvisor.py
+++ webadvisor.py
@@ -37,4 +37,4 @@ def page_not_found(e):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", port=8080, debug=False)
+    app.run(host="0.0.0.0", port=8082, debug=False)
